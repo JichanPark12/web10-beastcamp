e87f93216c572ecdd34877f388977e90b0968919
ParkTjgus <psh22112100@gmail.com>
2026-01-05 18:54:12 +0900

    chore: 프로젝트 기본 개발환경 설정
    - `backend` 폴더명을 `queue-backend`로 변경
    - nest로 `backend` 프로젝트 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [],
-        synchronize: false,
-      }),
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
