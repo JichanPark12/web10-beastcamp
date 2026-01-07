6a6f53baf7334cb527bc25d7f62eac0daca130b1
shininghyunho <gusgh3315@gmail.com>
2026-01-07 22:46:14 +0900

    feat: permance, seat, venue 엔티티 추가.

---
+++ backend/api-server/src/entities/seat.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  Unique,
+} from 'typeorm';
+import { Venue } from './venue.entity';
+
+@Entity('seats')
+@Unique('uk_venue_seat_location', ['venueId', 'seatLocation'])
+export class Seat {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'bigint', name: 'venue_id' })
+  venueId: number;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+    name: 'seat_location',
+    comment: '좌석위치',
+  })
+  seatLocation: string;
+
+  @ManyToOne(() => Venue)
+  @JoinColumn({ name: 'venue_id' })
+  venue: Venue;
+}
