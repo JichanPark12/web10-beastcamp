51ccea08c11921a0aae24ca2a373af0b9c30e079
shininghyunho <gusgh3315@gmail.com>
2026-01-08 14:34:57 +0900

    feat: GET /api/venues 구현.

--- backend/api-server/src/venues/venues.controller.ts
+++ backend/api-server/src/venues/venues.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { VenuesService } from './venues.service';
 import { CreateVenueRequestDto } from './dto/create-venue-request.dto';
 
@@ -9,5 +9,10 @@ export class VenuesController {
   @Post()
   async create(@Body() createVenueRequestDto: CreateVenueRequestDto) {
     return this.venuesService.create(createVenueRequestDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.venuesService.findAll();
   }
 }
--- backend/api-server/src/venues/venues.service.ts
+++ backend/api-server/src/venues/venues.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Venue } from './entities/venue.entity';
 import { CreateVenueRequestDto } from './dto/create-venue-request.dto';
+import { GetVenuesResponseDto } from './dto/get-venue-response.dto';
 
 @Injectable()
 export class VenuesService {
@@ -19,5 +20,10 @@ export class VenuesService {
     });
     const savedVenue = await this.venuesRepository.save(venue);
     return { id: savedVenue.id };
+  }
+
+  async findAll(): Promise<GetVenuesResponseDto> {
+    const venues = await this.venuesRepository.find();
+    return GetVenuesResponseDto.fromEntities(venues);
   }
 }
