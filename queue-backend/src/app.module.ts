8f3888bfeba779958f2d0fcfe227d320320f978e
viixix <hmu0516@unist.ac.kr>
2026-01-12 23:32:29 +0900

    feat: 대기열 관리 기능 추가
    - QueueModule, QueueController, QueueService 및 관련 테스트 파일 추가
    - 대기열 등록 및 토큰 발급 기능 구현
    - Swagger 문서화 및 쿠키 파서 설정 추가
    - AppModule에 QueueModule 및 redisConfig 로드 설정

--- queue-backend/src/app.module.ts
+++ queue-backend/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './redis/redis.module';
+import { QueueModule } from './queue/queue.module';
+import { redisConfig } from '@beastcamp/backend-config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      load: [redisConfig],
     }),
     RedisModule,
+    QueueModule,
   ],
 })
 export class AppModule {}
---
+++ queue-backend/src/queue/queue.service.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PROVIDERS, REDIS_KEYS } from '@beastcamp/shared-constants';
+import { Redis } from 'ioredis';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class QueueService {
+  constructor(@Inject(PROVIDERS.REDIS_QUEUE) private readonly redis: Redis) {}
+
+  async createQueueEntry(existingToken?: string) {
+    if (existingToken) {
+      const existingRank = await this.redis.zrank(
+        REDIS_KEYS.WAITING_QUEUE,
+        existingToken,
+      );
+      if (existingRank !== null) {
+        return { userId: existingToken, position: existingRank + 1 };
+      }
+    }
+    const userId = randomBytes(12).toString('base64url');
+    const score = Date.now();
+    await this.redis.zadd(REDIS_KEYS.WAITING_QUEUE, 'NX', score, userId);
+
+    console.log('REDIS_KEYS', REDIS_KEYS);
+    console.log('WAITING_QUEUE', REDIS_KEYS?.WAITING_QUEUE);
+    const rank = await this.redis.zrank(REDIS_KEYS.WAITING_QUEUE, userId);
+
+    return {
+      userId,
+      position: rank !== null ? rank + 1 : null,
+    };
+  }
+}
