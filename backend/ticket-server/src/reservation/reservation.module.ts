fd44c0c6f54044f736b4785989d652caa934ad3b
shininghyunho <gusgh3315@gmail.com>
2026-01-20 15:00:58 +0900

    chore: Reservation 모듈 구조만 추가.
    - 부모 모듈 : TicketScheduler.
    - 자식 모듈 : RedisModuel.

--- backend/ticket-server/src/reservation/reservation.module.ts
+++ backend/ticket-server/src/reservation/reservation.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PerformanceApiModule } from '../performance-api/performance-api.module';
 import { RedisModule } from '../redis/redis.module';
 import { ReservationService } from './reservation.service';
-import { TaskService } from './task.service';
 
 @Module({
-  imports: [PerformanceApiModule, RedisModule],
-  providers: [ReservationService, TaskService],
+  imports: [RedisModule],
+  providers: [ReservationService],
   exports: [ReservationService],
 })
 export class ReservationModule {}
