6ef69110881f3f069f516034356e407379c2b402
viixix <hmu0516@unist.ac.kr>
2026-01-20 09:49:57 +0900

    feat: JWT 설정 추가 및 큐 모듈에 통합
    - `jwt.config.ts`: JWT 설정을 위한 구성 추가
    - `index.ts`: JWT 설정을 내보내도록 수정
    - `app.module.ts`: JWT 설정을 전역 구성으로 추가
    - `queue.module.ts`: JWT 모듈을 비동기로 등록하여 설정을 적용

--- queue-backend/src/queue/queue.module.ts
+++ queue-backend/src/queue/queue.module.ts
@@ -4,9 +4,33 @@ import { QueueController } from './queue.controller';
 import { ScheduleModule } from '@nestjs/schedule';
 import { QueueWorker } from './queue.worker';
 import { QueueTrigger } from './queue.trigger';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import type { JwtSignOptions } from '@nestjs/jwt';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>('jwt.secret');
+        const expiresIn =
+          configService.get<JwtSignOptions['expiresIn']>('jwt.expiresIn');
+
+        if (!secret || !expiresIn) {
+          throw new Error(
+            'JWT_SECRET or JWT_EXPIRES_IN가 설정되지 않았습니다.',
+          );
+        }
+
+        return {
+          secret,
+          signOptions: { expiresIn },
+        };
+      },
+    }),
+  ],
   providers: [QueueService, QueueWorker, QueueTrigger],
   controllers: [QueueController],
 })
