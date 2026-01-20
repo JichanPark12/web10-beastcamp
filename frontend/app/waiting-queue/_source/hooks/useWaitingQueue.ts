1918049ef645e272ebfa31afee6916112275912a
ParkTjgus <psh22112100@gmail.com>
2026-01-21 02:23:55 +0900

    chore: 대기 완료 조건 변경
    - 변경 전: 현재 순번이 0일 떄
    - 변경 후: 현재 순번이 null이 아니고 0이하일때

--- frontend/app/waiting-queue/_source/hooks/useWaitingQueue.ts
+++ frontend/app/waiting-queue/_source/hooks/useWaitingQueue.ts
@@ -2,31 +2,24 @@ import { useEffect } from "react";
 import { useCurrentQueue, useEnterQueue } from "../queries/position";
 
 export const useWaitingQueue = () => {
-  // 1. 대기열 진입 (Mutation)
   const {
     mutate,
-    data: entryData, // ⭐️ 여기에 "최초 진입 순번"이 들어있습니다.
+    data: entryData,
     isSuccess: isEntrySuccess,
     isError,
   } = useEnterQueue();
 
   const { data: currentData } = useCurrentQueue(isEntrySuccess);
 
-  // 3. 마운트 시 자동 진입 시도
   useEffect(() => {
     mutate();
   }, [mutate]);
 
   return {
-    // 상태값들
-    isLoading: !isEntrySuccess && !isError, // 진입 중
-    isError, // 에러 발생
-
-    // 데이터들
-    initialOrder: entryData?.position, // ⭐️ 최초 순번 (ProgressBar의 max)
-    currentOrder: currentData?.position, // ⭐️ 현재 순번 (ProgressBar의 value)
-
-    // 완료 여부 (순번이 0이거나 완료 상태)
-    isFinished: currentData?.position === 0,
+    isLoading: !isEntrySuccess && !isError,
+    isError,
+    initialOrder: entryData?.position,
+    currentOrder: currentData?.position,
+    isFinished: currentData?.position && currentData?.position <= 0,
   };
 };
