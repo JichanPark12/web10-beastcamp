8d7aeec68d0dc19475693fb645268174f4db0446
viixix <hmu0516@unist.ac.kr>
2026-01-21 18:47:07 +0900

    fix: CI 실패 수정 (테스트 수정)

--- queue-backend/src/queue/heartbeat.service.spec.ts
+++ queue-backend/src/queue/heartbeat.service.spec.ts
@@ -1,3 +1,5 @@
+import { PROVIDERS } from '@beastcamp/shared-constants';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HeartbeatService } from './heartbeat.service';
 
@@ -5,14 +7,21 @@ describe('HeartbeatService', () => {
   let service: HeartbeatService;
 
   beforeEach(async () => {
+    const redisMock = { zadd: jest.fn() };
+    const configMock = { get: jest.fn().mockReturnValue(undefined) };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HeartbeatService],
+      providers: [
+        HeartbeatService,
+        { provide: ConfigService, useValue: configMock },
+        { provide: PROVIDERS.REDIS_QUEUE, useValue: redisMock },
+      ],
     }).compile();
 
     service = module.get<HeartbeatService>(HeartbeatService);
   });
 
-  it('should be defined', () => {
+  it('정의되어 있어야 한다', () => {
     expect(service).toBeDefined();
   });
 });
