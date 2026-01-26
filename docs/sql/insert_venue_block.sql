13aab41b3aa4b5ce9fa5ebaba460e815bad07e33
shininghyunho <gusgh3315@gmail.com>
2026-01-26 23:13:45 +0900

    feat: venues insert 쿼리 작성.
    - 기존에 추가한 SVG 파일 경로를 참조하여 추가.

---
+++ docs/sql/insert_venue_block.sql
@@ -0,0 +1,6 @@
+-- venues
+INSERT INTO venues (venue_name, block_map_url)
+VALUES ('인천 남동 체육관', '/static/svg/incheon_namdong_gymnasium.svg'),
+('대구 엑스코', '/static/svg/daegu_exco.svg'),
+('대전 컨벤션', '/static/svg/daejeon_convention.svg'),
+('일산 킨텍스', '/static/svg/ilsan_kintex.svg');
