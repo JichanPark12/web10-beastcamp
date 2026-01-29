c434f9f7f73043a609d8f3759996bca255cf928b
pjc0179 <pjc0179@gmail.com>
2026-01-29 19:25:49 +0900

    chore: 사용안되는 쿠키제거 코드 삭제

--- frontend/app/reservations/_source/hooks/useExitPage.ts
+++ frontend/app/reservations/_source/hooks/useExitPage.ts
@@ -6,8 +6,6 @@ export function useExitPage() {
   useEffect(() => {
     return () => {
       setToken(null);
-      document.cookie =
-        "waiting-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=queue.web10.site";
     };
   }, [setToken]);
 }
--- frontend/hooks/useResetAuthToken.ts
+++ frontend/hooks/useResetAuthToken.ts
@@ -5,8 +5,7 @@ export function useResetAuthToken() {
   const { setToken } = useAuth();
   useEffect(() => {
     setToken(null);
-    document.cookie =
-      "waiting-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=queue.web10.site";
+
     return () => {};
   }, [setToken]);
 }
