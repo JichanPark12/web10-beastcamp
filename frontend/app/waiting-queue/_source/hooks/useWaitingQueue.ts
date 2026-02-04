783f8c73c2ef10f98a5717ac47c9a70f8c4170b4
ParkTjgus <psh22112100@gmail.com>
2026-02-04 17:43:19 +0900

    chore: status default값 변경

--- frontend/app/waiting-queue/_source/hooks/useWaitingQueue.ts
+++ frontend/app/waiting-queue/_source/hooks/useWaitingQueue.ts
@@ -22,6 +22,6 @@ export const useWaitingQueue = () => {
     currentOrder: currentData?.position,
     isFinished: !!currentData?.token,
     token: currentData?.token ?? null,
-    status: currentData?.status ?? "closed",
+    status: currentData?.status ?? "open",
   };
 };
